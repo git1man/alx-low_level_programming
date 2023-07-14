cd7e4adf2981ceaf0c7e98297e9708cbe62a2e37
git1man <foxlsn125@gmail.com>
2023-07-14 09:50:34 -0700

    new fix to 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Prints the alphabet without q and e.
+ * main - Prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
     int i;
 
     for (i = 0; i < 10; i++)
     {
-        printf("%d\n", i);
+        printf("%d", i);
     }
-
+    printf("\n");
     return (0);
 }
